d7b1672f4018a82dc0286c46e2d1fbd47d5576c5
JonasAntonio <jayv.jonas@gmail.com>
2019-08-15 00:02:34 -0300

    Algum layout melhorado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,53 +2,88 @@
 @section('content')
     <div class="row">
         @if(!empty($file_content))
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-title text-center">
-                    <strong>{{$originalname}}</strong>
-                </div>
-                <div class="card-body">
-                    <pre>
-                        <code style="color: black !important;">
-                            <table class="code-border w100">
-                                <tbody>
-                                    @foreach ($file_content as $key => $file)
-                                    @php
-                                    {{
-                                        $key_color = 'pure-green';
-                                        $line_color = 'pastel-green';
-                                        if(!empty($file['warning'])) {
-                                            $key_color = 'pure-yellow';
-                                            $line_color = 'pastel-yellow';
-                                        }
-                                        if(!empty($file['danger'])) {
-                                            $key_color = 'pure-red';
-                                            $line_color = 'pastel-red';
-                                        }
-                                    }}    
-                                    @endphp
-                                    <tr class="unselectable">
-                                        <td class="{{$key_color}} key-style">{{(1+$key). " - "}}</td>
-                                        <td class="{{$line_color}}">{{$file['text']}}</td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </code>
-                    </pre>
-                </div>
-                <div class="card-footer">
-                    <strong>RESULTADOS</strong>
+        <div class="container">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Arquivos analisados</h1>
+                    </div>
+                    <div class="card-title text-center">
+                        <h3>{{$originalname}}</h3>
+                    </div>
+                    <div class="card-subtitle text-center">
+                        <h4>Análise do arquivo: {{$originalname}}</h4>
+                    </div>
+                    <div class="card-body">
+                        <pre>
+                            <code style="color: black !important;">
+                                <table class="code-border w100">
+                                    <tbody>
+                                        @foreach ($file_content as $key => $file)
+                                        @php
+                                        {{
+                                            $key_color = 'pure-green';
+                                            $line_color = 'pastel-green';
+                                            if(!empty($file['warning'])) {
+                                                $key_color = 'pure-yellow';
+                                                $line_color = 'pastel-yellow';
+                                            }
+                                            if(!empty($file['danger'])) {
+                                                $key_color = 'pure-red';
+                                                $line_color = 'pastel-red';
+                                            }
+                                        }}    
+                                        @endphp
+                                        <tr class="unselectable">
+                                            <td class="{{$key_color}} key-style">{{(1+$key). " - "}}</td>
+                                            <td class="{{$line_color}}">{{$file['text']}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </code>
+                        </pre>
+                    </div>
+                    <div class="card-footer">
+                        <strong>RESULTADOS</strong>
+                    </div>
                 </div>
             </div>
         </div>
         @endif
     </div>
-    <div>
-        <form action="/" method="post" enctype="multipart/form-data">
-            @csrf
-            <input type="file" name="file" id="file" class="form-control-file">
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+    <div class="row mt-2">
+        <div class="container">
+            <form action="/" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <h3>
+                                @if(empty($file_content))
+                                    Enviar arquivos
+                                @else
+                                    Enviar novos arquivos
+                                @endif
+                            </h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="col-md-12">
+                                        <input type="file" name="file" id="file" class="form-control-file" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary">Enviar <i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -3,24 +3,41 @@
     <head>
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script src="{{ asset('js/app.js') }}"></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>SegWeb</title>
     </head>
     <body>
-        <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">SegWeb</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Enviar arquivo <span class="sr-only">(página atual)</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid mt-2">
             @yield('content')
         </div>
     </body>
-    <footer>
-        <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+    <footer class="mt-2">
         <script src="{{ asset('js/jquery-input-file.js') }}"></script>
         <script type="text/javascript">
             j = jQuery.noConflict();
             j(document).ready(function() {
                 if(j(":file").length) {
-                    j(":file").filestyle({placeholder: "Nenhum arquivo foi selecionado!"});
+                    j(":file").filestyle({placeholder: "Selecione os arquivos"});
                 }
             })
         </script>
